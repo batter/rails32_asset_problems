fb4111a0f87cf8b3561862f1619461f90d291f6a
Ben Atkins <benatkins@fullbridge.com>
2012-05-07 14:47:04 -0400

    Brand new rails 3.2.2 project generated using the rails command line tool.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails32AssetProblems::Application.config.secret_token = 'df67697c03dc47304776f1189ad09eee81664d34d67b59335bc722c66fee8fc95cb8aa26e2ccd15941566bb5750215bc8119c78f1f30959cd3098ce9d2a1a3f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rails32AssetProblems::Application.config.session_store :cookie_store, key: '_rails32_asset_problems_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rails32AssetProblems::Application.config.session_store :active_record_store
